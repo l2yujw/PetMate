813116525b5e960c0a6aafda50cee0caaef91a5d
ryu <jeungwon28@gmail.com>
2023-08-24 23:16:12 +0900

    feat: Firebase 로그인 기능 틀

--- app/src/main/java/com/example/petmate/login/Login00Activity.kt
+++ app/src/main/java/com/example/petmate/login/Login00Activity.kt
@@ -4,23 +4,45 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.petmate.navigation.BottomNavActivity
 import com.example.petmate.databinding.ActivityLogin00Binding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
+// Tool -> Firebase -> Authentication -> 아무거나 클릭후 add
 class Login00Activity : AppCompatActivity() {
 
     private lateinit var binding : ActivityLogin00Binding
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLogin00Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLogin.setOnClickListener {
-            Log.d("ryu","D")
-            val intent = Intent(this, BottomNavActivity::class.java)
-            startActivity(intent)
+        auth = FirebaseAuth.getInstance()
 
+        binding.btnLogin.setOnClickListener {
+            var email = binding.etEmail.text.toString()
+            var password = binding.etPwd.text.toString()
+            login(email, password)
         }
+
+        binding.btnRegister.setOnClickListener{
+            var intent = Intent(this, Login10Activity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun login(email:String,password:String){
+        auth.signInWithEmailAndPassword(email,password) // 로그인
+            .addOnCompleteListener {
+                    result->
+                if(result.isSuccessful){
+                    var intent = Intent(this, BottomNavActivity::class.java)
+                    startActivity(intent)
+                }
+            }
     }
 }
---
+++ app/src/main/java/com/example/petmate/login/Login10Activity.kt
@@ -0,0 +1,53 @@
+package com.example.petmate.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.petmate.R
+import com.example.petmate.databinding.ActivityLogin00Binding
+import com.example.petmate.databinding.ActivityLogin10Binding
+import com.example.petmate.navigation.BottomNavActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class Login10Activity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLogin10Binding
+    lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLogin10Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnRegister.setOnClickListener{
+            var email = binding.etEmail.text.toString()
+            var password = binding.etPwd.text.toString()
+
+            register(email, password)
+        }
+    }
+
+    fun register(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email,password) // 회원 가입
+            .addOnCompleteListener {
+                    result ->
+                if(result.isSuccessful){
+                    Toast.makeText(this,"회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                    if(auth.currentUser!=null){
+                        finish()
+                    }
+                }
+                else if(result.exception?.message.isNullOrEmpty()){
+                    Toast.makeText(this,"오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this,"이미 존재하는 계정이거나, 회원가입에 실패했습니다.",Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+}
